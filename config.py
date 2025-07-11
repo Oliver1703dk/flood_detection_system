e000d83dee003709bfc19877b0371d039a366009
Oliver1703dk <Oliver1703dk@hotmail.com>
2025-07-11 11:06:02 +0200

    Change config from 4 to 3

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ IMAGE_NAME = "flood3"
 # when IMPORTANCE values are used to dynamically select models.
 # The following works: nano, small, xlarge
 model_size = "nano"
-model_number = 4
+model_number = 3
 
 # Importance weights used for dynamic model selection. They must sum to 1.
 IMPORTANCE = {
